8caaa13a90004602fbc2bdc400f6eb03310aca61
ao5og <itsadrianojog@gmail.com>
2023-03-14 17:50:29 +0200

    Update main.js

--- table_app/scripts/main.js
+++ table_app/scripts/main.js
@@ -1,19 +1,30 @@
-const dataChooser = document.querySelector("select");
+const smallDataUrl =
+	"http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​";
+const largeDataUrl =
+	"http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+
 const poemDisplay = document.querySelector("pre");
 const rowCount = document.querySelector("h3");
-dataChooser.addEventListener("change", () => {
-	const dataSize = dataChooser.value;
-	updateDisplay(dataSize);
-});
-let jsonData = {};
-function updateDisplay(dataSize) {
-	var dataURL =
-		"http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​";
 
-	if (dataSize == "Large data") {
-		dataURL =
-			"http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
-	}
+const smallButton = document.querySelector("#small-data");
+const largeButton = document.querySelector("#large-data");
+
+smallButton.addEventListener("click", () => {
+	updateDisplay(1);
+});
+
+largeButton.addEventListener("click", () => {
+	updateDisplay(2);
+});
+
+// store data right away
+let jsonData = {};
+
+// initial state
+updateDisplay(1);
+
+function updateDisplay(dataSize) {
+	let dataURL = dataSize == 2 ? largeDataUrl : smallDataUrl;
 
 	fetch(dataURL)
 		.then((response) => {
@@ -24,23 +35,16 @@ function updateDisplay(dataSize) {
 			return response.json();
 		})
 		.then((data) => {
-			initialize(data);
+			fillTable(data);
 			jsonData = data;
 		})
-		// .then((data) => {jsonData = data})
-
 		.catch((err) => console.error(`Fetch problem: ${err.message}`));
 }
 
-// initial state
-updateDisplay("Small data");
-dataChooser.value = "Small data";
-
-function initialize(data) {
+function fillPage(dataPage) {
 	let placeholder = document.querySelector("#data-output");
 	let out = "";
-	console.log("clearing table");
-	placeholder.innerHTML = out;
+
 	for (let row of data) {
 		out += `<tr>
               <td>${row.id}</td>
@@ -52,25 +56,53 @@ function initialize(data) {
 	}
 
 	placeholder.innerHTML = out;
-
+	console.log("Filled table with data.");
 	rowCount.innerText = `Number of rows: ${placeholder.rows.length}`;
-	console.log(data);
+	// console.log(data);
+}
+
+function fillTable(data) {
+	let placeholder = document.querySelector("#data-output");
+	let out = "";
+
+	for (let row of data) {
+		out += `<tr>
+              <td>${row.id}</td>
+              <td>${row.firstName}</td>
+              <td>${row.lastName}</td>
+              <td>${row.email}</td>
+              <td>${row.phone}</td>
+            </tr>`;
+	}
+
+	placeholder.innerHTML = out;
+	console.log("Filled table with data.");
+	rowCount.innerText = `Number of rows: ${placeholder.rows.length}`;
+	// console.log(data);
 }
 
 let arrowSorts = document.querySelectorAll(".arrow");
 
 for (let arrow of arrowSorts) {
-	let column = arrow.previousSibling.wholeText;
+	let column = arrow.previousElementSibling.textContent;
 	arrow.addEventListener("click", () => {
 		sortBy(column);
 	});
 }
 
-function sortByProperty(property) {
+// sorting logic
+function sortByPropertyAsc(property) {
 	return function (a, b) {
 		if (a[property] > b[property]) return 1;
 		else if (a[property] < b[property]) return -1;
+		return 0;
+	};
+}
 
+function sortByPropertyDesc(property) {
+	return function (a, b) {
+		if (a[property] < b[property]) return 1;
+		else if (a[property] > b[property]) return -1;
 		return 0;
 	};
 }
@@ -80,33 +112,29 @@ function sortBy(columnName) {
 	console.log(`adding new data sorted by ${columnName}`);
 
 	jsonData = jsonData.sort(sortByProperty(columnName));
-	initialize(jsonData);
+	fillTable(jsonData);
 }
 
-//
-class jsonHolder {
-	constructor(url) {
-		this.data = data;
-	}
+// pagination
+const rowsPerPage = 10;
+const rows = jsonData.length;
+const pages = Math.ceil(rows / rowsPage);
 
-	fillTable() {
-		let placeholder = document.querySelector("#data-output");
-		let out = "";
-		for (let row of data) {
-			out += `<tr>
-                <td>${row.id}</td>
-                <td>${row.firstName}</td>
-                <td>${row.lastName}</td>
-                <td>${row.email}</td>
-                <td>${row.phone}</td>
-              </tr>`;
-		}
-		placeholder.innerHTML = out;
+let currentPage = 1;
 
-		rowCount.innerText = `Number of rows: ${placeholder.rows.length}`;
-		console.log(data);
+const nav = document.querySelector("#pag-nav");
+
+function generateNav(pages) {
+	let out = "";
+	for (let page of pages) {
+		out += `<tr>
+			  <td>${row.id}</td>
+			  <td>${row.firstName}</td>
+			  <td>${row.lastName}</td>
+			  <td>${row.email}</td>
+			  <td>${row.phone}</td>
+			</tr>`;
 	}
 }
 
-// let j = jsonHolder()
-
+currentView = slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage);
